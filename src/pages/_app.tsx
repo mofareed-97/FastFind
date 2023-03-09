968e870da95d302f056d92e85c4c3e46af44ee61
mofareed-97 <m.fareeed1997@gmail.com>
2023-03-09 16:51:22 +0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react/types/theme";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
+// https://fakeapi.platzi.com/
+// https://dribbble.com/shots/19614098-Shopcart-E-Commerce-Product-Page
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from "@nextui-org/react";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>{CssBaseline.flush()}</Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
